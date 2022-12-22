c1270ca94171480d7fa675bd832eac90abdf97da
jango <jangoo042@gmail.com>
2022-12-22 07:15:20 +0100

    Adding Encryptin

---
+++ src/main/java/com/jango/customerrecordorganiser/util/EncryptData.java
@@ -0,0 +1,21 @@
+package com.jango.customerrecordorganiser.util;
+
+import jakarta.persistence.AttributeConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EncryptData implements AttributeConverter<String,String> {
+
+  @Autowired
+  EncryptionUtil encryptionUtil;
+
+  @Override
+  public String convertToDatabaseColumn(String s) {
+    return encryptionUtil.encrypt(s);
+  }
+
+  @Override
+  public String convertToEntityAttribute(String s) {
+    return encryptionUtil.decrypt(s);
+  }
+
+}
